8dbab3a7057cb83405b4676e55df30350fd8558d
vasyl onufriyev <onufriye@hoare7.cs.umsl.edu>
2019-03-07 20:46:58 -0600

    added sem

--- shared.h
+++ shared.h
@@ -1,8 +1,12 @@
 #ifndef SHARED_H
 
+#include <semaphore.h>
+
 typedef struct {
-    char rows[500][80];
+	char rows[500][80];
 	int rowcount;
+	sem_t pal;
+	sem_t nopal;
 } Shared;
 
 #define SHARED_H
